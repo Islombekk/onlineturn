670d21bb8203ba6cabad2b0d96eccf0389b0e9e9
Islombek <nazirbekovislombek@gmail.com>
2020-08-23 11:01:31 +0500

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 export const Navbar: React.FC = () => (
     <nav>
         <div className="nav-wrapper blue px1">
-            <a href="/" className="brand-logo">React + TypeScript</a>
+            <a href="/" className="brand-logo">Запись к врачу</a>
             <ul className="right hide-on-med-and-down">
                 <li><NavLink to="/">Запись к врачу</NavLink></li>
                 <li><NavLink to="/about">Расписание врача</NavLink></li>
